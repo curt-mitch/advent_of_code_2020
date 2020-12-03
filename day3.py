e3c03c94d44a479f026eb92c83a212f811b64bb9
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-12-03 10:17:48 -0800

    Day 3 part 2 solution

--- day3.py
+++ day3.py
@@ -1,19 +1,43 @@
 # https://adventofcode.com/2020/day/3
+from functools import reduce
 
 with open('day3_input.txt') as f:
     data = [line.strip() for line in f]
 
 
-i = 1
-j = 3
-tree_count = 0
-pattern_width = len(data[0])
-while i < len(data):
-    if data[i][j] == '#':
-        tree_count += 1
-    i += 1
-    j += 3
-    if j > (pattern_width - j):
-        j = j % pattern_width
+def count_trees(slope_map, down_move, right_move):
+    i = down_move
+    j = right_move
+    tree_count = 0
+    pattern_width = len(slope_map[0])
+    while i < len(slope_map):
+        if slope_map[i][j] == '#':
+            tree_count += 1
+        i += down_move
+        j += right_move
+        if j > (pattern_width - j):
+            j = j % pattern_width
+    return tree_count
 
-print(tree_count)
+
+# day 1 solution
+print(count_trees(data, 1, 3))
+
+
+# part 2
+# https://adventofcode.com/2020/day/3#part2
+
+
+slope_patterns = [  # [right move, down move]
+  [1, 1],
+  [1, 3],
+  [1, 5],
+  [1, 7],
+  [2, 1]
+]
+tree_counts = []
+
+for pattern in slope_patterns:
+    tree_counts.append(count_trees(data, pattern[0], pattern[1]))
+
+print(reduce((lambda x, y: x * y), tree_counts))
